1e3561567405e1b66cc469f8eb93572f267152d0
Reva1404 <revaverma06@outlook.com>
2026-06-09 23:03:51 +0530

    Delete python _task2.py

--- python _task2.py
+++
@@ -1,8 +0,0 @@
-d1={'Rahul':100,'mohan':78,'Shubham':45,'Nikhil':34,'Niyati':65}
-def above50(d1):
-    d2={}
-    for i in d1:
-        if d1[i]>50:
-            d2.update({i:d1[i]})
-    print(d2)
-above50(d1) 
