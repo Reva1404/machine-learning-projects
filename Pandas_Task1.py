afd627241a256a9aedec7c2e49fd229f1a02f38a
Reva1404 <revaverma06@outlook.com>
2026-06-09 23:03:09 +0530

    Delete Pandas_Task1.py

--- Pandas_Task1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = {'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],'Age': [25, 30, 35, 40, 45],
-        'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']}
-
-df= pd.DataFrame(data)
-print(df) #prints dataframe
-print(df.head(2)) #shows the first 2 rows 
-print("The mean of Age is",df['Age'].mean()) #calculates the mean of age coloumn
